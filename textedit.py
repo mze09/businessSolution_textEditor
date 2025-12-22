9ac63dca541fb50a5f6fdbae1d3f3adbe6f628bc
mze09 <113617375+mze09@users.noreply.github.com>
2025-12-22 10:27:07 -0500

    Update textedit.py

--- textedit.py
+++ textedit.py
@@ -1,1 +1,13 @@
 from tkinter import *
+from tkmacosx import Button
+
+root = Tk()
+root.title("Text Editor")
+
+root.geometry("800x400")
+
+searchbox = Text(root)
+
+searchbox.grid (row=2, column=1, columnWidth=2)
+
+root.mainloop()
