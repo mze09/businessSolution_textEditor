9646d9036320bf7e32d842423110609e661c0854
mze09 <113617375+mze09@users.noreply.github.com>
2025-12-22 10:57:39 -0500

    Update textedit.py

--- textedit.py
+++ textedit.py
@@ -4,10 +4,15 @@ from tkmacosx import Button
 root = Tk()
 root.title("Text Editor")
 
-root.geometry("800x400")
+root.geometry("600x200")
 
-searchbox = Text(root)
+label1 = Label(root, text="Company 1")
+label2 = Label(root, text="- Notes 1 [ Lorem ipsum d... ]")
+searchbox = Text(root, width=60, height=2)
 
-searchbox.grid (row=2, column=1, columnWidth=2)
+searchbox.grid(row=1, column=0)
+label1.grid(row=2, column=0)
+label2.grid(row=3, column=0)
+label1.config(font=("Arial", 25))
 
 root.mainloop()
