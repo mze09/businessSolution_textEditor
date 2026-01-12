7151156be75f083a4d6db4c35ea6b035b0e0f350
mze09 <113617375+mze09@users.noreply.github.com>
2026-01-12 10:37:44 -0500

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -5,4 +5,18 @@ root = Tk()
 root.title("Reading & Writing Files")
 root.geometry("300x150")
 
-textbox = Text(root, width=30, height=2)
+def writeFile():
+	with open("demofile.txt", "w") as f:
+		f.write("this is what will be inside of the file!")
+def readFile():
+	pass
+
+textbox = Text(root, width=30, height=4)
+writebutton = Button(root, text="Write To File", command=writeFile)
+readbutton = Button(root, text="Read From File", command=readFile)
+
+textbox.grid(row=1, column=1)
+writebutton.grid(row=2, column=1)
+readbutton.grid(row=3, column=1)
+
+root.mainloop()
