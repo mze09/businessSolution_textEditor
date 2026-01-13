ca4525a115f6d695f08bdbb583ea1d096158d0b8
mze09 <113617375+mze09@users.noreply.github.com>
2026-01-13 11:27:13 -0500

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -7,16 +7,14 @@ root.geometry("300x150")
 
 def writeFile():
 	with open("demofile.txt", "w") as f:
-		f.write("this is what will be inside of the file!")
 		text = textbox.get("1.0")
-		print("You entered:")
-		print(text)
+		f.write(text)
+		
 
 def readFile():
 	with open("demofile.txt", "r") as f:
 		content = f.read()
 		print(content)
-		textbox.insert("1.0", content)
 
 textbox = Text(root, width=30, height=4)
 writebutton = Button(root, text="Write To File", command=writeFile)
